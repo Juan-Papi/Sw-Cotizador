45860165b505fb9147be018da71b66892a52e742
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2024-06-23 17:06:10 -0400

    soluting a bug on chatAi, importing authModule

--- src/chat-ai/chat-ai.module.ts
+++ src/chat-ai/chat-ai.module.ts
@@ -4,11 +4,12 @@ import { ChatAiController } from './chat-ai.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatAi } from './entities/chat-ai.entity';
 import { ConfigService } from '@nestjs/config';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [ChatAiController],
   providers: [ChatAiService, ConfigService],
-  imports: [TypeOrmModule.forFeature([ChatAi])],
+  imports: [TypeOrmModule.forFeature([ChatAi]), AuthModule,],
   exports: [TypeOrmModule],
 })
 export class ChatAiModule {}
